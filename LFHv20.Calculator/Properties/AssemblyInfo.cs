87152b083fffdd9b64c290da40171e00227af29d
haryoursquare <mcleant91@gmail.com>
2020-06-05 14:52:16 +0100

    This is our project

---
+++ LFHv20.Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LFHv20.Calculator")]
+[assembly: AssemblyDescription("This product is bult by Timilehin, Ayorinde, Goodness and Justus(TAG-J) group for the purpose of making your mathematical calculations easy and accurate.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TAG-J group")]
+[assembly: AssemblyProduct("LFHv20.Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("42195d94-4f0e-4b52-8ec1-9654edae8349")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+
